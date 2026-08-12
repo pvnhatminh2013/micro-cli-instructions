566d91183b61c9754264cadf081fc5f721c05be2
pvnhatminh2013 <pvnhatminh2013@gmail.com>
2026-08-12 12:16:18 +0700

    Enable creation of multiple directories from arguments
    Modified the mkdir program to create multiple directories based on command line arguments.

--- mkdir.c
+++ mkdir.c
@@ -2,13 +2,11 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 int main(int argc, char *argv[]) {
-	if (argc < 2) {
-		perror("Not enough arguments.");
-		return 1;
-	}
-	if (mkdir(argv[1], 0777) == -1) {
-		perror("Error creating directory.\n");
-		return 1;
+	for (int i = 1; i < argc; i++) {
+		if (mkdir(argv[1], 0777) == -1) {
+			perror("Error creating directory.\n");
+			continue;
+		}
 	}
 	return 0;
 }
