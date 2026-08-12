10ed2c70b52a18a57f4eea846288f29942eca9eb
pvnhatminh2013 <pvnhatminh2013@gmail.com>
2026-08-12 12:42:20 +0700

    RMDIR, rmdir, rd command

---
+++ rmdir.c
@@ -0,0 +1,28 @@
+#define _XOPEN_SOURCE 500
+#include <stdio.h>
+#include <ftw.h>
+#include <unistd.h>
+int unlink_cb(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf)
+{
+    int rv = remove(fpath);
+    if (rv)
+        perror(fpath);
+    return rv;
+}
+int rmrf(char *path)
+{
+    return nftw(path, unlink_cb, 64, FTW_DEPTH | FTW_PHYS);
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s path\n", argv[0]);
+        return 1;
+    }
+    if (rmrf(argv[1]) == -1) {
+        perror("rmrf failed");
+        return 1;
+    }
+    return 0;
+}
