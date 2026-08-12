d3c6df1e6d802d05d5039bf19090c248a314fe1a
pvnhatminh2013 <pvnhatminh2013@gmail.com>
2026-08-12 10:56:19 +0700

    Add files via upload

---
+++ date.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <time.h>
+int main() {
+	time_t currentTime;
+	time_t error = -1;
+	if (time(&currentTime) == error) {
+		perror("Failed to get the current time.\n");
+		return 1;
+	}
+	printf("%s", ctime(&currentTime));
+	return 0;
+}
