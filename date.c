af774b4962ca6c3666547d669ac0c3019dafb976
pvnhatminh2013 <pvnhatminh2013@gmail.com>
2026-08-14 15:15:20 +0700

    Fix formatting issues in date.c

--- date.c
+++ date.c
@@ -4,7 +4,7 @@ int main() {
 	time_t currentTime;
 	time_t error = -1;
 	if (time(&currentTime) == error) {
-		perror("Failed to get the current time.\n");
+		perror("Failed to get the current time.");
 		return 1;
 	}
 	printf("%s", ctime(&currentTime));
