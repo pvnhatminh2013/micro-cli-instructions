c4443e0c94ee29b4206f97ba919e08209754b0df
pvnhatminh2013 <pvnhatminh2013@gmail.com>
2026-08-14 23:13:06 +0700

    Add 'choice' command to help output

--- help.c
+++ help.c
@@ -2,6 +2,7 @@
 int main() {
     printf(
     "beep - Plays a beep sound\n"
+    "choice prompt clause1 clause2 - Waits for the user to choose between y or n (case insensitive), If the user chooses y, execute clause1, else, execute clause2"
     "clear / cls - Clear screen\n"
     "copy / cp file1 file2 - Copy contents of file1 to file2\n"
     "date - Displays the date and time\n"
