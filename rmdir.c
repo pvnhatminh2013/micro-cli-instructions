ea494dc078ebe4e7e295179e0635403521014ad9
pvnhatminh2013 <pvnhatminh2013@gmail.com>
2026-08-14 14:51:23 +0700

    Refactor rmdir.c for improved error handling

--- rmdir.c
+++ rmdir.c
@@ -2,27 +2,26 @@
 #include <stdio.h>
 #include <ftw.h>
 #include <unistd.h>
-int unlink_cb(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf)
-{
+int unlink_cb(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf) {
     int rv = remove(fpath);
-    if (rv)
+    if (rv) {
         perror(fpath);
+    }
     return rv;
 }
-int rmrf(char *path)
-{
+int rmrf(char *path) {
     return nftw(path, unlink_cb, 64, FTW_DEPTH | FTW_PHYS);
 }
-
-int main(int argc, char *argv[])
-{
-    if (argc != 2) {
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
         fprintf(stderr, "usage: %s path\n", argv[0]);
         return 1;
     }
-    if (rmrf(argv[1]) == -1) {
-        perror("rmrf failed");
-        return 1;
+    for (int i = 1; i < argc; i++) {
+        if (rmrf(argv[i]) == -1) {
+            perror("rmrf failed");
+            return 1;
+        }
     }
     return 0;
 }
