6435172267819dd50e9d531dd79bf07b5eed9c36
pvnhatminh2013 <pvnhatminh2013@gmail.com>
2026-08-14 15:23:44 +0700

    Update help.c with new command descriptions

--- help.c
+++ help.c
@@ -9,9 +9,10 @@ int main() {
     "echo str1 str2 ... - Outputs str1, str2, ... (space separated)\n"
     "help - Displays information for all available commands\n"
     "mkdir / md dir1 dir2 ... - Makes directories dir1, dir2, ...\n"
-    "rename name1 name2 - Renames file or directory with name1 to name2\n"
+    "pwd - Print working directory\n"
+    "rename / ren name1 name2 - Renames file or directory with name1 to name2\n"
     "rmdir / rd dir1 dir2 ... - Removes dir1's inner contents and itself, same with dir2, etc."
     "time - Sets the system time\n"
-    "type file1 file2 ... - Output the inner contents of file1, file2,...\n"
+    "type / cat file1 file2 ... - Output the inner contents of file1, file2,...\n"
     );
 }
