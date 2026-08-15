b72690520c2ead2d861f9b01f6b72ead1bfe1201
pvnhatminh2013 <pvnhatminh2013@gmail.com>
2026-08-15 16:08:50 +0700

    Fix error message formatting in mkdir.c
    Removed period from error message in mkdir function.

--- mkdir.c
+++ mkdir.c
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[]) {
 	for (int i = 1; i < argc; i++) {
 		if (mkdir(argv[i], 0777) == -1) {
-			perror("Error creating directory.");
+			perror("Error creating directory");
 			continue;
 		}
 	}
