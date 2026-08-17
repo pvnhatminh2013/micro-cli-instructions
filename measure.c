ac9d75ce8df83084f33b1651018340df44597bf1
pvnhatminh2013 <pvnhatminh2013@gmail.com>
2026-08-17 20:02:04 +0700

    Measure command

---
+++ measure.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Wrong number of arguments.\n");
+    }
+    clock_t t;
+    t = clock();
+    system(argv[1]);
+    t = clock() - t;
+    double time_taken = ((double)t) / CLOCKS_PER_SEC; 
+    printf("%f\n", time_taken); 
+    return 0;
+}
